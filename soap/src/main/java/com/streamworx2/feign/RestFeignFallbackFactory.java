8ef8f9820a075bc979c1d8bd6ae99e576f847c4b
navirius <navirius.sutejo@greateasternlife.com>
2023-11-21 22:23:28 +0700

    fallback missing code

--- soap/src/main/java/com/streamworx2/feign/RestFeignFallbackFactory.java
+++ soap/src/main/java/com/streamworx2/feign/RestFeignFallbackFactory.java
@@ -1,10 +1,18 @@
 package com.streamworx2.feign;
 
+import com.streamworx2.data.feign.RequestModel;
+import com.streamworx2.data.feign.ResponseModel;
 import org.springframework.cloud.openfeign.FallbackFactory;
+
 
 public class RestFeignFallbackFactory implements FallbackFactory<RestFeignClient> {
     @Override
     public RestFeignClient create(Throwable cause) {
-        return null;
+        return new RestFeignClient() {
+            @Override
+            public ResponseModel getData(RequestModel request) {
+                return null;
+            }
+        };
     }
 }
